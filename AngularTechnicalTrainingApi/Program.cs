7047af6f18a3d96c1c86a3e0d66e7cda876a750d
adam watson <adamwatson08@gmail.com>
2022-06-04 12:38:36 -0500

    registered Imapper in startup

--- AngularTechnicalTrainingApi/Program.cs
+++ AngularTechnicalTrainingApi/Program.cs
@@ -1,5 +1,7 @@
 using AngularTechnicalTrainingApi.Data;
+using AngularTechnicalTrainingApi.Data.Configurations;
 using AngularTechnicalTrainingApi.Data.GenericRepository;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,12 +12,22 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 
+var config = new MapperConfiguration(cfg =>
+{
+    cfg.AddProfile(new AutoMapperConfiguration());
+});
+
+var mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+
 //dependency injections
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
