eae674ad257b3933155a8ad1b12616aa1930c7ff
adam watson <adamwatson08@gmail.com>
2022-06-03 20:55:55 -0500

    annotated model classes

--- AngularTechnicalTrainingApi.Domain/Models/Asset.cs
+++ AngularTechnicalTrainingApi.Domain/Models/Asset.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,23 @@ namespace AngularTechnicalTrainingApi.Domain.Models
 {
     public class Asset
     {
+        [Key]
+        public int TagID { get; set; }
 
+        [ForeignKey(nameof(AssetType))]
+        public int AssetTypeID { get; set; }
+        public AssetType AssetType { get; set; }
+
+        public string Description { get; set; }
+
+        [ForeignKey(nameof(Employee))]
+        public int EmployeeID { get; set; }
+        public Employee Employee { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public bool Retired { get; set; }
+
+        public DateTime DateRetired { get; set; }
     }
 }
