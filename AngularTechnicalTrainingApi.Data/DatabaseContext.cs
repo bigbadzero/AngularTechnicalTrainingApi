7e3ef6431ada89e29c102f0581b67c5bfd55e99b
adam watson <adamwatson08@gmail.com>
2022-06-03 20:37:40 -0500

    Add project files.

---
+++ AngularTechnicalTrainingApi.Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using AngularTechnicalTrainingApi.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularTechnicalTrainingApi.Data
+{
+    public class DatabaseContext: DbContext
+    {
+        public  DatabaseContext(DbContextOptions options): base(options)
+    {}
+    DbSet<AssetType> AssetTypes { get; set; }
+    DbSet<Employee> Employees { get; set; }
+}
+    
+
+}
