c859d5e56eb5798ed64c05922b55662d41c031ad
adam watson <adamwatson08@gmail.com>
2022-06-21 22:29:50 -0500

    added assetTypeController and EmployeeController

---
+++ AngularTechnicalTrainingApi/Controllers/AssetTypeController.cs
@@ -0,0 +1,29 @@
+﻿using AngularTechnicalTrainingApi.Data.GenericRepository;
+using AngularTechnicalTrainingApi.Dto;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularTechnicalTrainingApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AssetTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public AssetTypeController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAssetTypes()
+        {
+            var assetTypes = await _unitOfWork.AssetTypes.GetAll();
+            var result = _mapper.Map<IList<AssetTypeDTO>>(assetTypes);
+            return Ok(result);
+        }
+    }
+}
---
+++ AngularTechnicalTrainingApi/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using AngularTechnicalTrainingApi.Data.GenericRepository;
+using AngularTechnicalTrainingApi.Dto;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularTechnicalTrainingApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public EmployeeController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            var employees = await _unitOfWork.Employees.GetAll();
+            var results = _mapper.Map<IList<EmployeeDTO>>(employees);
+            return Ok(results);
+        }
+    }
+}
